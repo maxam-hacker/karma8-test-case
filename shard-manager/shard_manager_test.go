459aa2f2389050f1c51bbf7ce416edff7169bbf8
maxam-hacker <maxam.hacker@gmail.com>
2024-11-06 18:35:45 +0300

    the same

--- shard-manager/shard_manager_test.go
+++ shard-manager/shard_manager_test.go
@@ -1,130 +1,69 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"os"
+	"strconv"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
-
-	shardManagerApi "karma8-storage/api/shard-manager"
+	internalTypes "karma8-storage/internals/types"
 	"karma8-storage/shard-manager/replicas"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMain(m *testing.M) {
 	os.Setenv("SHARD_MANAGER_SERVICE_ADDR", "0.0.0.0")
 	os.Setenv("SHARD_MANAGER_SERVICE_PORT", "7788")
-	os.Setenv("REPLICAS_BASE_PATHS", "/tmp/karma8/test3-4-5/r1;/tmp/karma8/test3-4-5/r2")
+
+	os.Setenv("REPLICAS_BASE_PATH", "/tmp/karma8-integration-tests")
+	os.Setenv("REPLICAS_INDEX", "1")
+	os.Setenv("REPLICAS_PATHS", "r1;r2;r3")
+	replicas.Initialize()
+
+	go runRestServer()
+
+	time.Sleep(2 * time.Second)
 
 	m.Run()
 
-	os.RemoveAll("/tmp/karma8/test3-4-5")
+	os.RemoveAll("/tmp/karma8-integration-tests")
 
 	os.Exit(0)
 }
 
 func Test0001(t *testing.T) {
-	replicas.Initialize()
+	partData := []byte("Replica test1 data smple")
+	totalObjectSize := len(partData)
 
-	packetData := []byte("Test case 3 fake data")
+	partBucket := "karma8-test-case-0-bucket"
+	partKey := "replica-test-case-0-key"
 
-	uploadRequest := shardManagerApi.ShardManagerRequest{
-		UploadOpts: shardManagerApi.UploadOptions{
-			Packets: []shardManagerApi.PartPacket{
-				{
-					Bucket:          "test-case-3-bucket",
-					Key:             "test-case-3-key",
-					Data:            packetData,
-					Offset:          0,
-					PacketSize:      uint64(len(packetData)),
-					TotalObjectSize: uint64(len(packetData)),
-				},
-			},
-		},
+	objectPart := internalTypes.ObjectPart{
+		Bucket:            partBucket,
+		Key:               partKey,
+		Data:              &partData,
+		PartDataSize:      uint64(len(partData)),
+		TotalObjectOffset: 0,
+		TotalObjectSize:   uint64(totalObjectSize),
+		Opts:              internalTypes.ObjectPartOptions{},
 	}
 
-	uploadResponse, err := uploadPackage(&uploadRequest.UploadOpts)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, uploadResponse.Packets)
+	httpClient := &http.Client{}
 
-	downloadRequest := shardManagerApi.ShardManagerRequest{
-		DownloadOpts: shardManagerApi.DownloadOptions{
-			Packets: []shardManagerApi.PartPacket{
-				{
-					Bucket: "test-case-3-bucket",
-					Key:    "test-case-3-key",
-					Offset: 0,
-				},
-			},
-		},
-	}
-
-	downloadResponse, err := downloadPackage(&downloadRequest.DownloadOpts)
-	assert.NoError(t, err)
-	assert.NotEmpty(t, downloadResponse.Packets)
-	assert.Equal(t, packetData, downloadResponse.Packets[0].Data)
-}
-
-func Test0002(t *testing.T) {
-	replicas.Initialize()
-
-	go runRestServer()
-
-	time.Sleep(3 * time.Second)
-
-	packetData := []byte("Test case 4 fake data")
-
-	shardManagerUploadRequest := shardManagerApi.ShardManagerRequest{
-		UploadOpts: shardManagerApi.UploadOptions{
-			Packets: []shardManagerApi.PartPacket{
-				{
-					Bucket:          "test-case-4-bucket",
-					Key:             "test-case-4-key",
-					Data:            packetData,
-					Offset:          0,
-					PacketSize:      uint64(len(packetData)),
-					TotalObjectSize: uint64(len(packetData)),
-				},
-			},
-		},
-		DownloadOpts: shardManagerApi.DownloadOptions{},
-	}
-
-	requestReader, err := shardManagerUploadRequest.NewReader()
+	request, err := http.NewRequest("POST", "http://127.0.0.1:7788/shard-manager/object/part/upload", bytes.NewReader(*objectPart.Data))
 	assert.NoError(t, err)
 
-	uploadResponse, err := http.DefaultClient.Post("http://127.0.0.1:7788/shard-manager", "application/json", requestReader)
+	request.Header.Set("Content-Type", "application/octet-stream")
+	request.Header.Set("X-Karma8-Object-Bucket", objectPart.Bucket)
+	request.Header.Set("X-Karma8-Object-Key", objectPart.Key)
+	request.Header.Set("X-Karma8-Object-Part-Data-Size", strconv.Itoa(int(objectPart.PartDataSize)))
+	request.Header.Set("X-Karma8-Object-Total-Offset", strconv.Itoa(int(objectPart.TotalObjectOffset)))
+	request.Header.Set("X-Karma8-Object-Total-Size", strconv.Itoa(int(objectPart.TotalObjectSize)))
+
+	uploadResponse, err := httpClient.Do(request)
 	assert.NoError(t, err)
-	assert.Equal(t, 200, uploadResponse.StatusCode)
-
-	shardManagerDownloadRequest := shardManagerApi.ShardManagerRequest{
-		UploadOpts: shardManagerApi.UploadOptions{},
-		DownloadOpts: shardManagerApi.DownloadOptions{
-			Packets: []shardManagerApi.PartPacket{
-				{
-					Bucket: "test-case-4-bucket",
-					Key:    "test-case-4-key",
-					Offset: 0,
-				},
-			},
-		},
-	}
-
-	requestReader, err = shardManagerDownloadRequest.NewReader()
-	assert.NoError(t, err)
-
-	downloadResponse, err := http.DefaultClient.Post("http://127.0.0.1:7788/shard-manager", "application/json", requestReader)
-	assert.NoError(t, err)
-	assert.Equal(t, 200, downloadResponse.StatusCode)
-
-	dowloadResponseBytes := make([]byte, 4096)
-
-	n, _ := downloadResponse.Body.Read(dowloadResponseBytes)
-	assert.NotEqual(t, n, 0)
-
-	dowloadShardManagerResponse := shardManagerApi.ShardManagerResponse{}
-	dowloadShardManagerResponse.FromBytes(dowloadResponseBytes[0:n])
-	assert.Equal(t, len(dowloadShardManagerResponse.Packets), 1)
-	assert.Equal(t, dowloadShardManagerResponse.Packets[0].Data, packetData)
+	assert.Equal(t, uploadResponse.StatusCode, 200)
 }
